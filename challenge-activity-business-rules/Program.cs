949a34ec1a1c1357ce510cad5757deaddf23f927
worf <me@worf.win>
2023-05-14 19:46:45 +1000

    subscription expiry

---
+++ challenge-activity-business-rules/Program.cs
@@ -0,0 +1,22 @@
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+else if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+    Console.WriteLine("Renew now and save 10%!");
+}
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine("Your subscription expires within a day!");
+    Console.WriteLine("Renew now and save 20%");
+}
+else if (daysUntilExpiration == 0)
+{
+    Console.WriteLine("Your subscription has expired");
+}
