396f5015053604c65c123ec068943b1008628ed5
worf <me@worf.win>
2023-05-14 19:35:53 +1000

    homosex 3

--- 2-if-statements/Program.cs
+++ 2-if-statements/Program.cs
@@ -10,22 +10,34 @@ Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
 if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
 {
-    Console.WriteLine("You rolled doubles! +2 bonus to total!");
-    total += 2;
+    if ((roll1 == roll2) && (roll2 == roll3)) 
+    {
+        Console.WriteLine("You rolled triples! +6 bonus to total!");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles! +2 bonus to total!");
+        total += 2;
+    }
 }
 
-if ((roll1 == roll2) && (roll2 == roll3)) 
+
+if (total >= 16)
 {
-    Console.WriteLine("You rolled triples! +6 bonus to total!");
-    total += 6;
+    Console.WriteLine("You win a new car");
+}
+else if (total >= 10)
+{
+    Console.WriteLine("You win a new laptop");
+}
+else if (total == 7)
+{
+    Console.WriteLine("You win a trip for two!");
+}
+else
+{
+    Console.WriteLine("You win a kitten!");
 }
 
-if (total >= 15)
-{
-    Console.WriteLine("You win!");
-}
 
-if (total < 15)
-{
-    Console.WriteLine("Sorry, you lose.");
-}
