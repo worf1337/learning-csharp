326d5f7fd67830072539f66ce61f6dfaabe80d31
worf <me@worf.win>
2023-06-03 01:49:37 +1000

    arrays challenge

---
+++ arrays-challenge/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] fraudulentOrderIDs = new string[8];
+
+// declaring IDs
+
+fraudulentOrderIDs[1] = "B123";
+fraudulentOrderIDs[2] = "C234";
+fraudulentOrderIDs[3] = "A345";
+fraudulentOrderIDs[4] = "C15";
+fraudulentOrderIDs[5] = "B177";
+fraudulentOrderIDs[6] = "G3003";
+fraudulentOrderIDs[7] = "C235";
+fraudulentOrderIDs[8] = "B179";
+
